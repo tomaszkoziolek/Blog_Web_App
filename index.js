11f0a005d0409126daa370692e2054fb33fbf8bf
tomaszkoziolek <mes.tomaszkoziolek@gmail.com>
2024-03-27 12:18:40 +0100

    dodawanie i wyswietlanie postow 'draft'

--- index.js
+++ index.js
@@ -5,9 +5,25 @@ const app = express();
 app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+let posty = [];
+
 app.get("/", (req, res) => {
     res.render("index.ejs");
 });
+
+app.get("/createPost", (req, res) => {
+    res.render("createPost.ejs");
+});
+
+app.get("/viewPosts", (req, res) => {
+    res.render("viewPosts.ejs", {posty: posty});
+});
+
+app.post("/submit", (req, res) => {
+    posty.push(req.body["tekst"]);
+    console.log(posty);
+    res.redirect("/viewPosts");
+})
 
 app.listen(3000, () => {
     console.log("Listening on port 3000");
