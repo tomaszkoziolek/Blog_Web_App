b2801604a5eaa0a1c70da24c3acb3bce748ffe8c
tomaszkoziolek <mes.tomaszkoziolek@gmail.com>
2024-03-27 15:08:48 +0100

    usuwanie postow

--- index.js
+++ index.js
@@ -21,9 +21,15 @@ app.get("/viewPosts", (req, res) => {
 
 app.post("/submit", (req, res) => {
     posty.push(req.body["tekst"]);
-    console.log(posty);
+    // console.log(posty);
     res.redirect("/viewPosts");
-})
+});
+
+app.post("/delete/:id", (req, res) => {
+    const postId = req.params.id;
+    posty.splice(postId, 1);
+    res.redirect("/viewPosts");
+});
 
 app.listen(3000, () => {
     console.log("Listening on port 3000");
